e33f1c0693f278329bb123eb25fcc0bcd900d3e1
Boubacar Diallo <boubacar.diallo1@macaulay.cuny.edu>
2017-12-19 04:39:16 -0500

    results from functions show on results page

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 def index():
     if request.method=="GET":
         return render_template('index.html')
+
     filename = request.form['file']
     term = request.form['term']
-    print(ii.build_inverted_index(filename,0,8))
-    return render_template('index.html')
+    results = ii.search_dict(term, filename,0, 8)
+    print(results)
+    return render_template('results.html',results = results)
 
 
 app.run(debug=True, host = '127.0.0.1', port = 5000)
